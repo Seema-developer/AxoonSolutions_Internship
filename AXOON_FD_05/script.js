d3769111afdb574b0ac949fd5ebe42c7f26adabd
Seema-developer <metloseema@gmail.com>
2025-06-19 01:12:18 -1100

    added task 05

---
+++ AXOON_FD_05/script.js
@@ -0,0 +1,49 @@
+// hamburger on small devices
+const hamburger = document.getElementById('hamburger');
+const responsivenav = document.querySelector('.responsive-nav');
+const navLinks = document.querySelectorAll('.responsive-nav a');
+
+hamburger.addEventListener('click', function () {
+    responsivenav.classList.toggle('show');
+});
+
+// Close nav when any link is clicked
+navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+        responsivenav.classList.remove('show');
+    });
+});
+
+
+// cards scrolling animation (project cards)
+  window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".project-card");
+
+    cards.forEach((card, index) => {
+      const top = card.getBoundingClientRect().top;
+      const isVisible = top < window.innerHeight - 100;
+
+      if (isVisible && !card.classList.contains("animate")) {
+        setTimeout(() => {
+          card.classList.add("animate");
+        }, index * 200);
+      }
+    });
+  });
+
+// for expertise cards
+  window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".expertise-card");
+
+    cards.forEach((card, index) => {
+      const top = card.getBoundingClientRect().top;
+      const screenHeight = window.innerHeight;
+
+      if (top < screenHeight - 100) {
+        setTimeout(() => {
+          card.classList.add("animate");
+        }, index * 200);
+      }
+    });
+  });
+
